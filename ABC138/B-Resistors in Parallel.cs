2de714b58fff53daac80b76c9fa71650f8358b1c
rikupin1105 <41769991+rikupin1105@users.noreply.github.com>
2019-08-19 22:05:32 +0900

    ABC138

---
+++ ABC138/B-Resistors in Parallel.cs
@@ -0,0 +1,110 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using static System.Console;
+using static System.Math;
+
+namespace AtCoder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = ReadInt();
+            var a = ReadInts();
+
+
+            double tmp = 0.0;
+            for (int i = 0; i < n; i++)
+            {
+                tmp += 1.0 / a[i];
+            }
+            WriteLine(1.0/tmp);
+        }
+
+        private static string Read() { return ReadLine(); }
+        private static string[] Reads() { return (Read().Split()); }
+        private static int ReadInt() { return int.Parse(Read()); }
+        private static long ReadLong() { return long.Parse(Read()); }
+        private static double ReadDouble() { return double.Parse(Read()); }
+        private static int[] ReadInts() { return Array.ConvertAll(Read().Split(), int.Parse); }
+        private static long[] ReadLongs() { return Array.ConvertAll(Read().Split(), long.Parse); }
+        private static double[] ReadDoubles() { return Array.ConvertAll(Read().Split(), double.Parse); }
+
+        public static void debuglist(List<int> x)
+        {
+            foreach (var item in x)
+            {
+                WriteLine(item);
+            }
+        }
+        public static void debuglist(List<string> x)
+        {
+            foreach (var item in x)
+            {
+                WriteLine(item);
+            }
+        }
+        public static void debugarray(int[] x)
+        {
+            foreach (var item in x)
+            {
+                WriteLine(item);
+            }
+        }
+        public static void debugarray(string[] x)
+        {
+            foreach (var item in x)
+            {
+                WriteLine(item);
+            }
+        }
+
+        public static bool IsPrime(int num)
+        {
+            if (num < 2) return false;
+            else if (num == 2) return true;
+            else if (num % 2 == 0) return false;
+
+            double sqrtNum = Sqrt(num);
+            for (int i = 3; i <= sqrtNum; i += 2)
+            {
+                if (num % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        //最大公約数
+        public static int Gcd(int a, int b)
+        {
+            if (a < b)
+            {
+                return Gcd(b, a);
+            }
+            while (b != 0)
+            {
+                var reminder = a % b;
+                a = b;
+                b = reminder;
+            }
+            return a;
+        }
+
+        //最小公倍数
+        public static int Lcm(int a, int b)
+        {
+            return a * b / Gcd(a, b);
+        }
+
+        //文字数を数える
+        public static int CountChar(string s, char c)
+        {
+            return s.Length - s.Replace(c.ToString(), "").Length;
+        }
+    }
+}
