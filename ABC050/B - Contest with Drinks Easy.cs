6614bf3cc7ce55ad0bce1d44c862f8ded551c55b
rikupin1105 <41769991+rikupin1105@users.noreply.github.com>
2019-08-31 16:37:49 +0900

    ABC050

---
+++ ABC050/B - Contest with Drinks Easy.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using static System.Console;
+using static System.Math;
+
+namespace AtCoder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = Read();
+            for (int i = 0; i < s.Length; i++)
+            {
+                for (int i1 = i+1; i1 < s.Length; i1++)
+                {
+                    if (s[i] == s[i1])
+                    {
+                        WriteLine("no");
+                        return;
+                    }
+                }
+            }
+            WriteLine("yes");
+        }
+
+        private static string Read() { return ReadLine(); }
+        private static string[] Reads() { return (Read().Split()); }
+        private static int ReadInt() { return int.Parse(Read()); }
+        private static long ReadLong() { return long.Parse(Read()); }
+        private static double ReadDouble() { return double.Parse(Read()); }
+        private static int[] ReadInts() { return Array.ConvertAll(Read().Split(), int.Parse); }
+        private static long[] ReadLongs() { return Array.ConvertAll(Read().Split(), long.Parse); }
+        private static double[] ReadDoubles() { return Array.ConvertAll(Read().Split(), double.Parse); }
+
+        //最大公約数
+
+        public static int Gcd(int a, int b)
+        {
+            if (a < b)
+            {
+                return Gcd(b, a);
+            }
+            while (b != 0)
+            {
+                var reminder = a % b;
+                a = b;
+                b = reminder;
+            }
+            return a;
+        }
+
+        //最小公倍数
+        public static int Lcm(int a, int b)
+        {
+            return a * b / Gcd(a, b);
+        }
+
+        //文字数を数える
+        public static int CountChar(string s, char c)
+        {
+            return s.Length - s.Replace(c.ToString(), "").Length;
+        }
+    }
+}
