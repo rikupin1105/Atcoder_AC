119c09ae4b0b60d939387b1ae8bccc56ac16ece0
rikupin1105 <41769991+rikupin1105@users.noreply.github.com>
2019-11-03 15:47:14 +0900

    ABC035

---
+++ ABC035/A - テレビ.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using static System.Console;
+using static System.Math;
+
+namespace AtCoder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] wh = ReadDoubles();
+            if (wh[0] / wh[1] == 16.0 / 9)
+            {
+                WriteLine("16:9");
+            }
+            else
+            {
+                WriteLine("4:3");
+            }
+        }
+        private static string Read() { return ReadLine(); }
+        private static string[] Reads() { return (Read().Split()); }
+        private static int ReadInt() { return int.Parse(Read()); }
+        private static long ReadLong() { return long.Parse(Read()); }
+        private static double ReadDouble() { return double.Parse(Read()); }
+        private static int[] ReadInts() { return Array.ConvertAll(Read().Split(), int.Parse); }
+        private static long[] ReadLongs() { return Array.ConvertAll(Read().Split(), long.Parse); }
+        private static double[] ReadDoubles() { return Array.ConvertAll(Read().Split(), double.Parse); }
+    }
+}
