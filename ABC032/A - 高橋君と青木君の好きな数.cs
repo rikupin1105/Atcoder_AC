1e6140faba8ad1e2f711078a82543e195e67c2af
rikupin1105 <41769991+rikupin1105@users.noreply.github.com>
2019-11-03 15:57:19 +0900

    ABC032

---
+++ ABC032/A - 高橋君と青木君の好きな数.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using static System.Console;
+using static System.Math;
+
+namespace AtCoder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = ReadInt();
+            var b = ReadInt();
+            var n = ReadInt();
+
+            var i = n;
+            while (true)
+            {
+                if(i%a==0 && i % b == 0)
+                {
+                    WriteLine(i);
+                    return;
+                }
+                i++;
+            }
+        }
+        private static string Read() { return ReadLine(); }
+        private static string[] Reads() { return (Read().Split()); }
+        private static int ReadInt() { return int.Parse(Read()); }
+        private static long ReadLong() { return long.Parse(Read()); }
+        private static double ReadDouble() { return double.Parse(Read()); }
+        private static int[] ReadInts() { return Array.ConvertAll(Read().Split(), int.Parse); }
+        private static long[] ReadLongs() { return Array.ConvertAll(Read().Split(), long.Parse); }
+        private static double[] ReadDoubles() { return Array.ConvertAll(Read().Split(), double.Parse); }
+    }
+}
