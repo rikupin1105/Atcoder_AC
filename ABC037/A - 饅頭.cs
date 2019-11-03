d0dda1c45f7922c92fbbef6a117ab8a64ee20cd4
rikupin1105 <41769991+rikupin1105@users.noreply.github.com>
2019-11-03 15:43:10 +0900

    ABC037

---
+++ ABC037/A - 饅頭.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using static System.Console;
+using static System.Math;
+
+namespace AtCoder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] abc = ReadInts();
+            WriteLine(abc[2] / Min(abc[0], abc[1]));
+        }
+
+
+        private static string Read() { return ReadLine(); }
+        private static string[] Reads() { return (Read().Split()); }
+        private static int ReadInt() { return int.Parse(Read()); }
+        private static long ReadLong() { return long.Parse(Read()); }
+        private static double ReadDouble() { return double.Parse(Read()); }
+        private static int[] ReadInts() { return Array.ConvertAll(Read().Split(), int.Parse); }
+        private static long[] ReadLongs() { return Array.ConvertAll(Read().Split(), long.Parse); }
+        private static double[] ReadDoubles() { return Array.ConvertAll(Read().Split(), double.Parse); }
+
+
+    }
+}
