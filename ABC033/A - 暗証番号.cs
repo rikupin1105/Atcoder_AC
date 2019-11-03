2cf18fdc4026001eb26e96aa1a5d4074d9ae82f7
rikupin1105 <41769991+rikupin1105@users.noreply.github.com>
2019-11-03 15:52:02 +0900

    ABC033

---
+++ ABC033/A - 暗証番号.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using static System.Console;
+using static System.Math;
+
+namespace AtCoder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = Read();
+            if (n[0] == n[1] && n[0] == n[2] && n[0] == n[3])
+            {
+                WriteLine("SAME");
+            }
+            else
+            {
+                WriteLine("DIFFERENT");
+            }
+        }
+        private static string Read() { return ReadLine(); }
+        private static string[] Reads() { return (Read().Split()); }
+        private static int ReadInt() { return int.Parse(Read()); }
+        private static long ReadLong() { return long.Parse(Read()); }
+        private static double ReadDouble() { return double.Parse(Read()); }
+        private static int[] ReadInts() { return Array.ConvertAll(Read().Split(), int.Parse); }
+        private static long[] ReadLongs() { return Array.ConvertAll(Read().Split(), long.Parse); }
+        private static double[] ReadDoubles() { return Array.ConvertAll(Read().Split(), double.Parse); }
+    }
+}
