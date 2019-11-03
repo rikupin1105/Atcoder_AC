37921b62b23b464cc63084e0b27311e4a019cb4a
rikupin1105 <41769991+rikupin1105@users.noreply.github.com>
2019-11-03 15:43:02 +0900

    ABC036

---
+++ ABC036/A - お茶.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using static System.Console;
+using static System.Math;
+
+namespace AtCoder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ab = ReadInts();
+            var ans = ab[1] / ab[0];
+            if (ab[1] % ab[0] != 0) ans++;
+            WriteLine(ans);
+        }
+
+
+        private static string Read() { return ReadLine(); }
+        private static string[] Reads() { return (Read().Split()); }
+        private static int ReadInt() { return int.Parse(Read()); }
+        private static long ReadLong() { return long.Parse(Read()); }
+        private static double ReadDouble() { return double.Parse(Read()); }
+        private static int[] ReadInts() { return Array.ConvertAll(Read().Split(), int.Parse); }
+        private static long[] ReadLongs() { return Array.ConvertAll(Read().Split(), long.Parse); }
+        private static double[] ReadDoubles() { return Array.ConvertAll(Read().Split(), double.Parse); }
+
+
+    }
+}
