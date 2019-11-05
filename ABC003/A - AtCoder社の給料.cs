4022ab34a43d483d068deac1cbad2f7ab805c63a
rikupin1105 <41769991+rikupin1105@users.noreply.github.com>
2019-11-05 13:57:48 +0900

    ABC003

---
+++ ABC003/A - AtCoder社の給料.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using static System.Console;
+using static System.Math;
+
+namespace At
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = ReadInt();
+            var list = new List<int>();
+            for (int i = 1; i <= n; i++)
+            {
+                list.Add(i * 10000);
+            }
+            WriteLine(list.Average());
+        }
+
+        private static string Read() { return ReadLine(); }
+        private static string[] Reads() { return (Read().Split()); }
+        private static int ReadInt() { return int.Parse(Read()); }
+        private static long ReadLong() { return long.Parse(Read()); }
+        private static double ReadDouble() { return double.Parse(Read()); }
+        private static int[] ReadInts() { return Array.ConvertAll(Read().Split(), int.Parse); }
+        private static long[] ReadLongs() { return Array.ConvertAll(Read().Split(), long.Parse); }
+        private static double[] ReadDoubles() { return Array.ConvertAll(Read().Split(), double.Parse); }
+    }
+}
