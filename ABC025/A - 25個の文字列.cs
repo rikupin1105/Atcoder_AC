042f63323ffd81f715e0980da6a07e2bcb97588b
rikupin1105 <41769991+rikupin1105@users.noreply.github.com>
2019-11-05 13:49:47 +0900

    ABC025

---
+++ ABC025/A - 25個の文字列.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using static System.Console;
+using static System.Math;
+
+namespace AtCoder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = Read();
+            var n = ReadInt();
+            var list = new List<string>();
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    list.Add(s[i].ToString() + s[j].ToString());
+                }
+            }
+            WriteLine(list[n-1]);
+        }
+        private static string Read() { return ReadLine(); }
+        private static string[] Reads() { return (Read().Split()); }
+        private static int ReadInt() { return int.Parse(Read()); }
+        private static long ReadLong() { return long.Parse(Read()); }
+        private static double ReadDouble() { return double.Parse(Read()); }
+        private static int[] ReadInts() { return Array.ConvertAll(Read().Split(), int.Parse); }
+        private static long[] ReadLongs() { return Array.ConvertAll(Read().Split(), long.Parse); }
+        private static double[] ReadDoubles() { return Array.ConvertAll(Read().Split(), double.Parse); }
+    }
+}
