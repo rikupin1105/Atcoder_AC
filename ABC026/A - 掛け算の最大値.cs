5e86343c4b1723e5ada62c9d19b7051ac6dac07c
rikupin1105 <41769991+rikupin1105@users.noreply.github.com>
2019-11-05 13:45:35 +0900

    ABC026

---
+++ ABC026/A - 掛け算の最大値.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using static System.Console;
+using static System.Math;
+
+namespace AtCoder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = ReadInt();
+            var list = new List<int>();
+            for (int i = 1; i <= a; i++)
+            {
+                var x = i;
+                var y = a - i;
+                list.Add(x * y);
+            }
+            WriteLine(list.Max());
+        }
+        private static string Read() { return ReadLine(); }
+        private static string[] Reads() { return (Read().Split()); }
+        private static int ReadInt() { return int.Parse(Read()); }
+        private static long ReadLong() { return long.Parse(Read()); }
+        private static double ReadDouble() { return double.Parse(Read()); }
+        private static int[] ReadInts() { return Array.ConvertAll(Read().Split(), int.Parse); }
+        private static long[] ReadLongs() { return Array.ConvertAll(Read().Split(), long.Parse); }
+        private static double[] ReadDoubles() { return Array.ConvertAll(Read().Split(), double.Parse); }
+    }
+}
