3ba778349167b2b0da18d8d7de5e42b808329292
rikupin1105 <41769991+rikupin1105@users.noreply.github.com>
2020-02-21 00:42:13 +0900

    ABC154

---
+++ AtCoder Beginner Contest/ABC154/A - Remaining Balls.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using static System.Console;
+using static System.Math;
+
+namespace AtCoder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var st = Reads();
+            var ab = ReadInts();
+            var u = Read();
+
+            if (u == st[0])
+            {
+                WriteLine(ab[0] - 1 + " " + ab[1]);
+            }
+            else
+            {
+                WriteLine(ab[0] + " " + (ab[1] - 1));
+            }
+        }
+        private static string Read() { return ReadLine(); }
+        private static string[] Reads() { return (Read().Split()); }
+        private static int ReadInt() { return int.Parse(Read()); }
+        private static long ReadLong() { return long.Parse(Read()); }
+        private static double ReadDouble() { return double.Parse(Read()); }
+        private static int[] ReadInts() { return Array.ConvertAll(Read().Split(), int.Parse); }
+        private static long[] ReadLongs() { return Array.ConvertAll(Read().Split(), long.Parse); }
+        private static double[] ReadDoubles() { return Array.ConvertAll(Read().Split(), double.Parse); }
+    }
+}
