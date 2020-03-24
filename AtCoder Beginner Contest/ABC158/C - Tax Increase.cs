ddf19ca395e749bcbd80c3ca64acead7eb0a9465
rikupin1105 <41769991+rikupin1105@users.noreply.github.com>
2020-03-24 17:06:23 +0900

    ABC158

---
+++ AtCoder Beginner Contest/ABC158/C - Tax Increase.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using static System.Console;
+using static System.Math;
+
+namespace AtCoder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var AB = ReadInts();
+            for (int i = 1; i <= 1009; i++)
+            {
+                if (Floor(i * 0.08) == AB[0])
+                {
+                    if (Floor(i * 0.1) == AB[1])
+                    {
+                        WriteLine(i);
+                        return;
+                    }
+                }
+            }
+            WriteLine(-1);
+        }
+
+        private static string Read() { return ReadLine(); }
+        private static string[] Reads() { return (Read().Split()); }
+        private static int ReadInt() { return int.Parse(Read()); }
+        private static long ReadLong() { return long.Parse(Read()); }
+        private static double ReadDouble() { return double.Parse(Read()); }
+        private static int[] ReadInts() { return Array.ConvertAll(Read().Split(), int.Parse); }
+        private static long[] ReadLongs() { return Array.ConvertAll(Read().Split(), long.Parse); }
+        private static double[] ReadDoubles() { return Array.ConvertAll(Read().Split(), double.Parse); }
+    }
+}
