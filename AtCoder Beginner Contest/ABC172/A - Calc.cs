97e7ba7b898c11e1e680f4b979689cb7f8d2d785
rikupin1105 <41769991+rikupin1105@users.noreply.github.com>
2020-07-07 15:48:34 +0900

    ABC172

---
+++ AtCoder Beginner Contest/ABC172/A - Calc.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Diagnostics.Contracts;
+using System.Dynamic;
+using System.Globalization;
+using System.IO;
+using System.IO.Compression;
+using System.IO.Pipes;
+using System.Linq;
+using System.Net;
+using System.Numerics;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using static System.Console;
+using static System.Math;
+
+namespace AtCoder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = ReadInt();
+            WriteLine((a) + (a * a) + (a * a * a));
+
+        }
+
+
+        private static string Read() { return ReadLine(); }
+        private static string[] Reads() { return (Read().Split()); }
+        private static int ReadInt() { return int.Parse(Read()); }
+        private static long ReadLong() { return long.Parse(Read()); }
+        private static double ReadDouble() { return double.Parse(Read()); }
+        private static int[] ReadInts() { return Array.ConvertAll(Read().Split(), int.Parse); }
+        private static long[] ReadLongs() { return Array.ConvertAll(Read().Split(), long.Parse); }
+        private static double[] ReadDoubles() { return Array.ConvertAll(Read().Split(), double.Parse); }
+    }
+}
