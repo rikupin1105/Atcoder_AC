0583b05b523b0302efe7791f0eee01822bfbcbc0
rikupin1105 <41769991+rikupin1105@users.noreply.github.com>
2020-07-07 16:04:27 +0900

    ABC171

---
+++ AtCoder Beginner Contest/ABC171/A - αlphabet.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using static System.Console;
+using static System.Math;
+
+namespace AtCoder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = Read();
+            if (a == a.ToUpper())
+                WriteLine("A");
+            else
+                WriteLine("a");
+        }
+
+        private static string Read() { return ReadLine(); }
+        private static string[] Reads() { return (Read().Split()); }
+        private static int ReadInt() { return int.Parse(Read()); }
+        private static long ReadLong() { return long.Parse(Read()); }
+        private static double ReadDouble() { return double.Parse(Read()); }
+        private static int[] ReadInts() { return Array.ConvertAll(Read().Split(), int.Parse); }
+        private static long[] ReadLongs() { return Array.ConvertAll(Read().Split(), long.Parse); }
+        private static double[] ReadDoubles() { return Array.ConvertAll(Read().Split(), double.Parse); }
+    }
+}
