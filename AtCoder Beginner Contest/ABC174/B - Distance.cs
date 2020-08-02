2544a56a147f2f40d76174b986ec90d63afcf6b0
rikupin <41769991+rikupin@users.noreply.github.com>
2020-08-03 00:46:25 +0900

    ABC174

---
+++ AtCoder Beginner Contest/ABC174/B - Distance.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.IO.Pipes;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using static System.Console;
+using static System.Math;
+
+namespace AtCoder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ND = ReadInts();
+            var ans = 0;
+            for (int i = 0; i < ND[0]; i++)
+            {
+                var XY = ReadLongs();
+                var distance = Sqrt((XY[0] * XY[0]) +( XY[1] * XY[1]));
+                if (distance <= ND[1]) ans++;
+            }
+            WriteLine(ans);
+        }
+
+        private static string Read() { return ReadLine(); }
+        private static string[] Reads() { return (Read().Split()); }
+        private static int ReadInt() { return int.Parse(Read()); }
+        private static long ReadLong() { return long.Parse(Read()); }
+        private static double ReadDouble() { return double.Parse(Read()); }
+        private static int[] ReadInts() { return Array.ConvertAll(Read().Split(), int.Parse); }
+        private static long[] ReadLongs() { return Array.ConvertAll(Read().Split(), long.Parse); }
+        private static double[] ReadDoubles() { return Array.ConvertAll(Read().Split(), double.Parse); }
+    }
+}
