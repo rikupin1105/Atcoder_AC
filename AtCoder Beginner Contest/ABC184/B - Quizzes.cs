5a55408be1028c9524bd9e6d8165b6d695e1fa02
rikupin <41769991+rikupin@users.noreply.github.com>
2020-11-26 15:57:45 +0900

    ABC184

---
+++ AtCoder Beginner Contest/ABC184/B - Quizzes.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using static System.Console;
+using static System.Math;
+
+namespace AtCoder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var NX = ReadInts();
+            var ans = NX[1];
+            var S = Read();
+            for (int i = 0; i < NX[0]; i++)
+            {
+                var result = S[i];
+                if (result == 'x')
+                {
+                    ans = Max(0, ans - 1);
+                }
+                else
+                {
+                    ans++;
+                }
+            }
+            WriteLine(ans);
+        }
+
+
+        private static string Read() { return ReadLine(); }
+        private static string[] Reads() { return (Read().Split()); }
+        private static int ReadInt() { return int.Parse(Read()); }
+        private static long ReadLong() { return long.Parse(Read()); }
+        private static double ReadDouble() { return double.Parse(Read()); }
+        private static int[] ReadInts() { return Array.ConvertAll(Read().Split(), int.Parse); }
+        private static long[] ReadLongs() { return Array.ConvertAll(Read().Split(), long.Parse); }
+        private static double[] ReadDoubles() { return Array.ConvertAll(Read().Split(), double.Parse); }
+    }
+}
