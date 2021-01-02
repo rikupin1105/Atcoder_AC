13671b5acbe86d802fe95444110ff6d87943f7dd
rikupin <41769991+rikupin@users.noreply.github.com>
2021-01-02 23:45:14 +0900

    ABC187

---
+++ AtCoder Beginner Contest/ABC187/A - Large Digits.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using static System.Math;
+
+namespace AtCoder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var AB = Reads();
+            var A = AB[0].ToCharArray().Select(x => int.Parse(x.ToString())).Sum();
+            var B = AB[1].ToCharArray().Select(x => int.Parse(x.ToString())).Sum();
+            Console.WriteLine(Max(A,B));
+        }
+        private static string Read() { return Console.ReadLine(); }
+        private static string[] Reads() { return (Read().Split()); }
+        private static int ReadInt() { return int.Parse(Read()); }
+        private static long ReadLong() { return long.Parse(Read()); }
+        private static double ReadDouble() { return double.Parse(Read()); }
+        private static int[] ReadInts() { return Array.ConvertAll(Read().Split(), int.Parse); }
+        private static long[] ReadLongs() { return Array.ConvertAll(Read().Split(), long.Parse); }
+        private static double[] ReadDoubles() { return Array.ConvertAll(Read().Split(), double.Parse); }
+    }
+}
