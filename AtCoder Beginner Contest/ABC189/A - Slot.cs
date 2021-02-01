05573096d20b7cf8a76f5d4a0c5e1cb5187114f5
kaminaka rikuya <rikupin1105@gmail.com>
2021-02-01 14:52:51 +0900

    ABC189

---
+++ AtCoder Beginner Contest/ABC189/A - Slot.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using static System.Console;
+using static System.Math;
+
+namespace AtCoder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var C = Read();
+            if(C[0]==C[1] && C[0] == C[2])
+            {
+                Console.WriteLine("Won");
+            }
+            else
+            {
+                Console.WriteLine("Lost");
+            }
+        }
+
+        private static string Read() { return ReadLine(); }
+        private static string[] Reads() { return (Read().Split()); }
+        private static int ReadInt() { return int.Parse(Read()); }
+        private static long ReadLong() { return long.Parse(Read()); }
+        private static double ReadDouble() { return double.Parse(Read()); }
+        private static int[] ReadInts() { return Array.ConvertAll(Read().Split(), int.Parse); }
+        private static long[] ReadLongs() { return Array.ConvertAll(Read().Split(), long.Parse); }
+        private static double[] ReadDoubles() { return Array.ConvertAll(Read().Split(), double.Parse); }
+    }
+}
---
+++ AtCoder Beginner Contest/ABC189/B - Alcoholic.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using static System.Console;
+using static System.Math;
+
+namespace AtCoder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var NX = ReadInts();
+            var al = 0.0;
+            for (int i = 0; i < NX[0]; i++)
+            {
+                var VP = ReadDoubles();
+                al += VP[0] * VP[1];
+                if (al > NX[1] * 100)
+                {
+                    WriteLine(i + 1);
+                    return;
+                }
+            }
+            WriteLine(-1);
+        }
+
+        private static string Read() { return ReadLine(); }
+        private static string[] Reads() { return (Read().Split()); }
+        private static int ReadInt() { return int.Parse(Read()); }
+        private static long ReadLong() { return long.Parse(Read()); }
+        private static double ReadDouble() { return double.Parse(Read()); }
+        private static int[] ReadInts() { return Array.ConvertAll(Read().Split(), int.Parse); }
+        private static long[] ReadLongs() { return Array.ConvertAll(Read().Split(), long.Parse); }
+        private static double[] ReadDoubles() { return Array.ConvertAll(Read().Split(), double.Parse); }
+    }
+}
