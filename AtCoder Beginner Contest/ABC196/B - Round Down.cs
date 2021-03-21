26d600dc249a97c26e273a66c6c8301426760bfb
kaminaka rikuya <rikupin1105@gmail.com>
2021-03-21 11:08:08 +0900

    ABC196

---
+++ AtCoder Beginner Contest/ABC196/B - Round Down.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+//using System.Numerics;
+using System.Text;
+using static System.Console;
+using static System.Math;
+
+namespace AtCoder
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var X = Read();
+            var ans = "";
+            for (int i = 0; i < X.Length; i++)
+            {
+                if (X[i] == '.')
+                {
+                    break;
+                }
+                else
+                {
+                    ans += X[i];
+                }
+            }
+            WriteLine(ans);
+        }
+        private static string Read() { return ReadLine(); }
+        private static string[] Reads() { return (Read().Split()); }
+        private static int ReadInt() { return int.Parse(Read()); }
+        private static long ReadLong() { return long.Parse(Read()); }
+        private static double ReadDouble() { return double.Parse(Read()); }
+        private static int[] ReadInts() { return Array.ConvertAll(Read().Split(), int.Parse); }
+        private static long[] ReadLongs() { return Array.ConvertAll(Read().Split(), long.Parse); }
+        private static double[] ReadDoubles() { return Array.ConvertAll(Read().Split(), double.Parse); }
+    }
+}
