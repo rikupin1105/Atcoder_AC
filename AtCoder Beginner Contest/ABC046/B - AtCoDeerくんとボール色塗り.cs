ddc0904f9e3c65f95346a0d357a487cda5022e19
kaminaka rikuya <rikupin1105@gmail.com>
2022-09-29 16:57:37 +0900

    ABC046

---
+++ AtCoder Beginner Contest/ABC046/B - AtCoDeerくんとボール色塗り.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text.RegularExpressions;
+using static System.Console;
+using static System.Math;
+
+namespace AtCoder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nk = ReadInts();
+            var n = nk[0];
+            var k = nk[1];
+
+            long ans = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (i == 0) ans = k;
+                else
+                {
+                    ans = ans * (k - 1);
+                }
+            }
+            WriteLine(ans);
+        }
+        private static string Read() { return ReadLine(); }
+        private static string[] Reads() { return Read().Split(); }
+        private static decimal ReadDecimal() { return decimal.Parse(Read()); }
+        private static int ReadInt() { return int.Parse(Read()); }
+        private static long ReadLong() { return long.Parse(Read()); }
+        private static double ReadDouble() { return double.Parse(Read()); }
+        private static int[] ReadInts() { return Array.ConvertAll(Read().Split(), int.Parse); }
+        private static long[] ReadLongs() { return Array.ConvertAll(Read().Split(), long.Parse); }
+        private static double[] ReadDoubles() { return Array.ConvertAll(Read().Split(), double.Parse); }
+    }
+}
