711f89671fa8ceae6512b791050f30ca76680899
kaminaka rikuya <rikupin1105@gmail.com>
2022-09-30 12:57:43 +0900

    ABC255

---
+++ AtCoder Beginner Contest/ABC255/A - You should output ARC, though this is ABC..cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text.RegularExpressions;
+using static System.Console;
+using static System.Math;
+
+namespace AtCoder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rc = ReadInts();
+            var a = ReadInts();
+            var aa = ReadInts();
+            var lists = new List<List<int>>();
+            lists.Add(a.ToList());
+            lists.Add(aa.ToList());
+
+            WriteLine(lists[rc[0]-1][rc[1]-1]);
+        }
+        private static string Read() { return ReadLine(); }
+        private static string[] Reads() { return Read().Split(); }
+        private static decimal ReadDecimal() { return decimal.Parse(Read()); }
+        private static int ReadInt() { return int.Parse(Read()); }
+        private static long ReadLong() { return long.Parse(Read()); }
+        private static double ReadDouble() { return double.Parse(Read()); }
+        private static int[] ReadInts() { return Array.ConvertAll(Read().Split(), int.Parse); }
+        private static long[] ReadLongs() { return Array.ConvertAll(Read().Split(), long.Parse); }
+        private static double[] ReadDoubles() { return Array.ConvertAll(Read().Split(), double.Parse); }
+    }
+}
