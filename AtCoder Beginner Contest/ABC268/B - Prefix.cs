1b312f7326084e21ab8fa4a23c46226078749fea
kaminaka rikuya <rikupin1105@gmail.com>
2022-10-04 11:21:54 +0900

    ABC268

---
+++ AtCoder Beginner Contest/ABC268/B - Prefix.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text.RegularExpressions;
+using static System.Console;
+using static System.Math;
+
+namespace AtCoder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = Read();
+            var t = Read();
+
+            if (s.Count() > t.Count())
+            {
+                WriteLine("No");
+                return;
+            }
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i]!=t[i])
+                {
+                    WriteLine("No");
+                    return;
+                }
+            }
+            WriteLine("Yes");
+        }
+        private static string Read() { return ReadLine(); }
+        private static string[] Reads() { return Read().Split(); }
+        private static decimal ReadDecimal() { return decimal.Parse(Read()); }
+        private static int ReadInt() { return int.Parse(Read()); }
+        private static long ReadLong() { return long.Parse(Read()); }
+        private static double ReadDouble() { return double.Parse(Read()); }
+        private static int[] ReadInts() { return Array.ConvertAll(Read().Split(), int.Parse); }
+        private static long[] ReadLongs() { return Array.ConvertAll(Read().Split(), long.Parse); }
+        private static double[] ReadDoubles() { return Array.ConvertAll(Read().Split(), double.Parse); }
+    }
+}
