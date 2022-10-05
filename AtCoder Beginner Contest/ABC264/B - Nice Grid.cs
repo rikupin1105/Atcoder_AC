268f6979b5b8b09bf72d1a6e4126915c21721deb
kaminaka rikuya <rikupin1105@gmail.com>
2022-10-05 14:31:23 +0900

    ABC264

---
+++ AtCoder Beginner Contest/ABC264/B - Nice Grid.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text.RegularExpressions;
+using static System.Console;
+using static System.Math;
+
+namespace AtCoder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rc = ReadInts();
+            if (Max(Abs(rc[0] - 8), Abs(rc[1] - 8))% 2 ==0)
+            {
+                WriteLine("white");
+            }
+            else
+            {
+                WriteLine("black");
+            }
+        }
+
+        private static string Read() { return ReadLine(); }
+        private static string[] Reads() { return Read().Split(); }
+        private static decimal ReadDecimal() { return decimal.Parse(Read()); }
+        private static int ReadInt() { return int.Parse(Read()); }
+        private static long ReadLong() { return long.Parse(Read()); }
+        private static double ReadDouble() { return double.Parse(Read()); }
+        private static int[] ReadInts() { return Array.ConvertAll(Read().Split(), int.Parse); }
+        private static long[] ReadLongs() { return Array.ConvertAll(Read().Split(), long.Parse); }
+        private static double[] ReadDoubles() { return Array.ConvertAll(Read().Split(), double.Parse); }
+    }
+}
